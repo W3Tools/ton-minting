f2c7654dd8d200754bc01783e166f5e3d70d4486
W3Tools <ritter@w3tools.app>
2023-08-02 16:56:26 +0800

    add sbt single

---
+++ scripts/deploy_sbt_single.ts
@@ -0,0 +1,51 @@
+import { Address, Builder, Cell } from 'ton';
+import { TonCenter } from './lib/ton-center';
+
+export class DeployNFTSingle extends TonCenter {
+    constructor() {
+        super();
+    }
+
+    getDeployData(owner: Address) {
+        let contentCell = this.encodeContent('https://file-8sgle4kt.w3tools.app/ton/1.json');
+
+        let royaltyCell: Builder = new Builder();
+        royaltyCell.storeUint(10, 16);
+        royaltyCell.storeUint(20, 16);
+        royaltyCell.storeAddress(owner);
+
+        let dataCell: Builder = new Builder();
+        dataCell.storeAddress(owner); // owner address
+        dataCell.storeAddress(owner); // editor_address
+        dataCell.storeRef(contentCell); // content
+        dataCell.storeAddress(owner); // authority_address
+        dataCell.storeUint(0, 64); // revoked_at
+
+        return dataCell.endCell();
+    }
+
+    async main() {
+        await this.getWallet();
+
+        const dataCell = this.getDeployData(this.wallet.address);
+
+        // get new contract address
+        const newContract = await this.createContract(dataCell, 'sbt-single.cell');
+
+        // get wallet params
+        const walletC = this.ton.open(this.wallet);
+        const seqno = await walletC.getSeqno();
+        console.log(`seqno: ${seqno}`);
+
+        // deploy
+        const sender = walletC.sender(this.keyPair.secretKey);
+        const contract = this.ton.open(newContract);
+        await contract.sendDeploy(sender);
+
+        // waiting for tx
+        await this.awaitTransaction(walletC, seqno);
+        console.log('transaction confirmed!');
+    }
+}
+
+new DeployNFTSingle().main();
